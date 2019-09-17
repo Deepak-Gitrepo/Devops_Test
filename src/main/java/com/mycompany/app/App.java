f09fa3993265f758eaae0b85de0ed8af095edb84
Deepak-Gitrepo <55058252+Deepak-Gitrepo@users.noreply.github.com>
2019-09-17 21:37:26 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(new App().getMessage());
     }
 
